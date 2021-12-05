698724074ce51b3e20477a67dc00f27753972a28
kawasaki <yomitanmasa3523@gmail.com>
2021-12-05 11:16:35 +0900

    lambda用に大きく作り直し

--- handler/callbackHandler.go
+++ handler/callbackHandler.go
@@ -1,37 +1,62 @@
 package handler
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/line/line-bot-sdk-go/linebot"
 	"line_bot_api_search_restaurants/service"
+	"line_bot_api_search_restaurants/domain"
 
 )
 
-func LineHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
+
+func LineHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	
+	log.Print(request.Headers)
+	log.Print(request.Body)
 	fmt.Println()
+
 	bot, err := service.GetLineToken()
+
 	if err != nil {
 		// Do something when something bad happened.
 		log.Fatal(err)
-		return 
+		return events.APIGatewayProxyResponse{
+			// サーバー側のエラー
+			Body:       fmt.Sprintf(`{"message:":"%s"}`+"\n", http.StatusText(http.StatusInternalServerError)),
+			StatusCode: http.StatusInternalServerError,
+			}, nil
 	}
 
-	//リクエスト取得
-	events, err := bot.ParseRequest(r)
-	if err != nil {
-		if err == linebot.ErrInvalidSignature {
-			w.WriteHeader(400)
-		} else {
-			w.WriteHeader(500)
-		}
-		return 
+	if !validateSignature(os.Getenv("SECRET_TOKEN"), request.Headers["X-Line-Signature"], []byte(request.Body)) {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusBadRequest,
+			Body:       fmt.Sprintf(`{"message":"%s"}`+"\n", linebot.ErrInvalidSignature.Error()),
+		}, nil
 	}
-	for _, event := range events {
 
+	var api domain.Api
+	
+	if err := json.Unmarshal([]byte(request.Body), &api); err != nil {
+		log.Print(err)
+		// クライアントのエラーを返す
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusBadRequest,
+			Body:       fmt.Sprintf(`{"message":"%s"}`+"\n", http.StatusText(http.StatusBadRequest)),
+		}, nil
+	}
+
+	for _, event := range api.Events {
+fmt.Println(event)
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 			// メッセージがテキスト形式の場合
@@ -47,5 +72,23 @@ func LineHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	
-	return 
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+	}, nil
+}
+
+
+func validateSignature(channelSecret string, signature string, body []byte) bool {
+	decoded, err := base64.StdEncoding.DecodeString(signature)
+	if err != nil {
+		return false
+	}
+
+	hash := hmac.New(sha256.New, []byte(channelSecret))
+	_, err = hash.Write(body)
+	if err != nil {
+		return false
+	}
+
+	return hmac.Equal(decoded, hash.Sum(nil))
 }
--- main.go
+++ main.go
@@ -1,24 +1,11 @@
 package main
 
 import (
-	"net/http"
-	"context"
-	"log"
-	"github.com/aws/aws-lambda-go/lambdacontext"
+
 	"github.com/aws/aws-lambda-go/lambda"
 	"line_bot_api_search_restaurants/handler"
 )
 
-
 func main() {
-    lambda.Start(helloHandler)
-}
-
-func helloHandler(ctx context.Context) {
-	// http.HandleFunc("/", handler.LineHandler)
-	handler := http.HandlerFunc(handler.LineHandler)
-	http.Handle("/", handler)
-	http.ListenAndServe(":8080", nil)
-	lc, _ := lambdacontext.FromContext(ctx)
-  log.Print(lc.Identity.CognitoIdentityPoolID)
+	lambda.Start(handler.LineHandler)
 }
